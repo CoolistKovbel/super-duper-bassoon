0e1a3748e099b2284d3ef898d007e23d83542953
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-06-17 02:53:10 +0300

    work

--- app/(sites)/swap/page.tsx
+++ app/(sites)/swap/page.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>
+    <main className='w-full min-h-screen p-10'>
 
       <header className='w-full p-10'>
         <h1 className='text-2xl font-bold'>Swap</h1>
         <p className='text-sm'>Feature coming soon</p>
       </header>
 
-    </div>
+    </main>
   )
 }
 
